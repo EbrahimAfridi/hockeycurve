c4916125081627d1cfa192355625fe43f6146c49
ebrahim afridi <afridiebr@student.iul.ac.in>
2024-09-17 23:32:42 +0530

    add schema setup

---
+++ backend/prisma/migrations/20240917180017_init/migration.sql
@@ -0,0 +1,20 @@
+-- CreateEnum
+CREATE TYPE "Priority" AS ENUM ('LOW', 'MEDIUM', 'HIGH');
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "dueDate" TIMESTAMP(3) NOT NULL,
+    "priority" "Priority" NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "snoozed" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Task_title_key" ON "Task"("title");
