02d0e8603484a8c265e7179fd42320e4ea59d21e
ebrahim afridi <afridiebr@student.iul.ac.in>
2024-09-18 01:23:58 +0530

    add: update task end-point

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import {Hono} from 'hono'
 import {PrismaClient} from "@prisma/client/edge"
 import {withAccelerate} from "@prisma/extension-accelerate"
+import {UpdateTaskPayload} from "../types";
 
 
 const app = new Hono<{
@@ -45,6 +46,40 @@ app.post("/create", async (c) => {
     } catch (error) {
         console.error(error);
         return c.text("Error while creating a task.", 411);
+    }
+});
+
+// 3. Update a task
+app.patch("/update/:id", async (c) => {
+    const {title, description, dueDate, priority, completed, snoozed} = await c.req.json();
+    const id = Number(c.req.param("id"));
+    console.log("id", id);
+
+    // Create an updateData object based on the fields provided
+    const updateData: UpdateTaskPayload = {};
+    if (title) updateData.title = title;
+    if (description) updateData.description = description;
+    if (dueDate) updateData.dueDate = new Date(dueDate).toISOString(); // Converting Date object into string
+    if (priority) updateData.priority = priority;
+    if (typeof completed === 'boolean') updateData.completed = completed;
+    if (typeof snoozed === 'boolean') updateData.snoozed = snoozed;
+
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    try {
+        const updatedTask = await prisma.task.update({
+            where: {id: id},
+            data: updateData,
+        });
+        console.log(updatedTask);
+        return c.json({task: updatedTask}, 200);
+    } catch (error) {
+        console.error(error);
+        return c.text("Error while updating task.", 500);
+    } finally {
+        console.log("Updating task...");
     }
 });
 
---
+++ backend/types.ts
@@ -0,0 +1,8 @@
+export interface UpdateTaskPayload {
+    title?: string;
+    description?: string;
+    dueDate?: string; // ISO date format as a string
+    priority?: 'LOW' | 'MEDIUM' | 'HIGH'; // Match your enum values
+    completed?: boolean;
+    snoozed?: boolean;
+}
